87622ee1d2d2d580aea5b5dd80b1292302185452
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-27 11:20:34 +0300

    commit tree 40261170db4499332f7048b223c85e5331adc03f

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    // Write your algorithm here
+    const seenNumbers = {};
+    for (const number of array) {
+        const complement = target - number;
+        if (seenNumbers[complement]) return true;
+        seenNumbers[number] = true;
+    }
+    return false;
 }
-
+// for (let i = 0; i < array.length; i++) {
+//     const targetNum = array[i];
+//     const neededValues = target - targetNum;
+//     const index2 = targetSum[neededValues];
+//     if (index2 != null) {
+//         return (index2, i);
+//     } else {
+//         targetSum(targetNum) = i;
+//     }
+// }
 /* 
   Write the Big O time complexity of your function here
 */
@@ -16,19 +32,25 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+    // add your own custom tests in here
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([1, 4, 3, 4], 8));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+    console.log("Expecting: true");
+    console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
